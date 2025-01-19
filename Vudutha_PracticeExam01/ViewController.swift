18344a1f683d39969559c49680b656916b629fd5
Mahithavudutha <s566410@nwmissouriedu>
2025-01-19 07:46:24 -0600

    Initial Commit

---
+++ Vudutha_PracticeExam01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vudutha_PracticeExam01
+//
+//  Created by Mahitha Vudutha on 1/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
